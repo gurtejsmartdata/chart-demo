1b63732cc8f4e02369c52cbd4559957fca6e3668
Henry Yu <hazyfish@outlook.com>
2021-02-08 23:23:17 -0800

    Add nav

--- components/App.tsx
+++ components/App.tsx
@@ -1,17 +1,20 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ChartListScreen from './ChartListScreen';
+
+const Stack = createStackNavigator();
 
 export default class App extends React.Component {
   render() {
     return (
-      <FlatList
-        data={[
-          "Line Chart",
-          "Bar Chart",
-          "Pie Chart",
-          "Scatter Plot Chart"
-        ]}
-        renderItem={({item}) => <Button title={item} onPress={() => {setTimeout(0)}}/>} />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={ChartListScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
---
+++ components/ChartListScreen.tsx
@@ -0,0 +1,30 @@
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import React from "react";
+import { Button, FlatList } from "react-native";
+
+interface ListItem {
+  id: string;
+  name: string;
+  route: string;
+}
+
+export default class ChartListScreen extends React.Component<{ navigation: NavigationProp<any> }> {
+
+  readonly items: ListItem[] = [
+    { id: '0', name: "Bar Chart", route: "" },
+    { id: '1', name: "Pie Chart", route: "" },
+    { id: '2', name: "Line Chart", route: "" },
+    { id: '3',  name: "Scatter Plot Chart", route: "" }
+  ];
+
+  render() {
+    const nav = this.props.navigation;
+
+    return (
+      <FlatList
+        data={this.items}
+        renderItem={({ item }) => <Button title={item.name} onPress={() => nav.navigate(item.route)} />}
+      />
+    );
+  }
+}
